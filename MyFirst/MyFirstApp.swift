37dc57251d21424c7efccbd6af3d7970c48af9dd
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-12 20:08:07 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Tetsuro Watanabe on 2021/02/12.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
