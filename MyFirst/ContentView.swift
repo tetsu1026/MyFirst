f033187a97428b620e2f9e36c484d35a807e714b
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-13 12:53:37 +0900

    first commit

--- MyFirst/ContentView.swift
+++ MyFirst/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var outputText = "Hello, World!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text(outputText)
+                .font(.largeTitle)
+                .padding()
+            Button(action: {
+                outputText = "Hi, Swift!"
+            }) {
+                Text("切替ボタン")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
+
